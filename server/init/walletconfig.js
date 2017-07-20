094c4ec6002afd0586c68e4354f486ab080b618b
Steve Conrad <sconrad1@gmail.com>
2017-07-20 15:11:27 -0500

    Delete walletconfig.js

--- server/init/walletconfig.js
+++
@@ -1,3 +0,0 @@
-export const password = 'Healgoma9';
-export const seed = 'word leader arrest object arm argue lady enhance mountain august aware topple';
-export const ethereumEndpoint = 'http://52.179.124.154:8545';
